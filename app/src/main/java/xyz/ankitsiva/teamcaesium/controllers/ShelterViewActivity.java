67a8634055b7b13d44a7497f31aa242a624e72e5
kevin400085 <kevin400085@yahoo.com>
2018-03-03 14:14:36 -0500

    Firebase database connected and able to be read from

--- app/src/main/java/xyz/ankitsiva/teamcaesium/controllers/ShelterViewActivity.java
+++ app/src/main/java/xyz/ankitsiva/teamcaesium/controllers/ShelterViewActivity.java
@@ -1,18 +1,67 @@
 package xyz.ankitsiva.teamcaesium.controllers;
 
 import android.content.Intent;
+import android.nfc.Tag;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import xyz.ankitsiva.teamcaesium.R;
 
+
 public class ShelterViewActivity extends AppCompatActivity {
 
+    public static class Post {
+
+        public String name;
+
+        public Post(String name) {
+            this.name = name;
+        }
+
+    }
+    public static final String TAG = "ShelterViewActivity";
+    GenericTypeIndicator<ArrayList<HashMap<String, Object>>> t =
+            new GenericTypeIndicator<ArrayList<HashMap<String, Object>>>() {};
+    private DatabaseReference mDatabase;
+    //private ArrayList<TextView> shelters = new ArrayList<TextView>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_view);
+
+        mDatabase = FirebaseDatabase.getInstance().getReferenceFromUrl(
+                "https://cs2340-49af4.firebaseio.com/");
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                ArrayList<HashMap<String, Object>> values =  dataSnapshot.getValue(t);
+                Log.d(TAG, "Value is: " + values);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
+
     }
 
     //Will need bundle or extras along with the Intent to pass shelter information over
@@ -20,4 +69,6 @@ public class ShelterViewActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ShelterContentActivity.class);
         startActivity(intent);
     }
+
+
 }
