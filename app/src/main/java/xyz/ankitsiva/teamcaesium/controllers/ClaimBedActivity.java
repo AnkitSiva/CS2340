8569727e371806cacd06369e56356016413a92df
16saadc <16saadc@gmail.com>
2018-03-26 19:34:05 -0400

    claim bed button working

--- app/src/main/java/xyz/ankitsiva/teamcaesium/controllers/ClaimBedActivity.java
+++ app/src/main/java/xyz/ankitsiva/teamcaesium/controllers/ClaimBedActivity.java
@@ -2,15 +2,26 @@ package xyz.ankitsiva.teamcaesium.controllers;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
 
 import xyz.ankitsiva.teamcaesium.R;
 import xyz.ankitsiva.teamcaesium.model.Shelter;
 import xyz.ankitsiva.teamcaesium.model.Vacancy;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ClaimBedActivity extends AppCompatActivity {
 
@@ -19,6 +30,8 @@ public class ClaimBedActivity extends AppCompatActivity {
     private Shelter shelter;
     private TextView mView;
     private Vacancy vacancy;
+    private EditText mEdit;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,10 @@ public class ClaimBedActivity extends AppCompatActivity {
         vacancy = shelter.getVacancies();
         mView = findViewById(R.id.Vacancies);
         mView.setText("Vacancies:   " + vacancy.getBeds());
+        mEdit = findViewById(R.id.NumberBeds);
+        mDatabase = FirebaseDatabase.getInstance().getReferenceFromUrl(
+                "https://cs2340-49af4.firebaseio.com/");
+
     }
 
     @Override
@@ -45,5 +62,30 @@ public class ClaimBedActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+
+
+    public void claimBed(View view) {
+        String text = mEdit.getText().toString();
+        int num = 0;
+        try {
+            num = Integer.parseInt(text);
+        } catch (Exception e) {
+            finish();
+        }
+        if (!vacancy.claimBed(num)) {
+            Context context = getApplicationContext();
+            CharSequence text2 = "Unable to claim beds";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text2, duration);
+            toast.show();
+
+        } else {
+            //shelter.writeToParcel(Parcel.obtain(), Parcelable.PARCELABLE_WRITE_RETURN_VALUE);
+            finish();
+            startActivity(getIntent());
+        }
     }
 }
--- app/src/main/java/xyz/ankitsiva/teamcaesium/model/Vacancy.java
+++ app/src/main/java/xyz/ankitsiva/teamcaesium/model/Vacancy.java
@@ -41,8 +41,8 @@ public class Vacancy implements Parcelable{
     };
 
     public boolean claimBed(int amount) {
-        if (beds - amount >= 0) {
-            beds = beds = amount;
+        if (beds - amount >= 0 && amount > 0) {
+            beds = beds - amount;
             return true;
         } else {
             return false;
@@ -50,7 +50,7 @@ public class Vacancy implements Parcelable{
     }
 
     public boolean releaseBed(int amount) {
-        if (beds + amount <= maximum) {
+        if (beds + amount <= maximum && amount > 0) {
             beds = beds + amount;
             return true;
         } else {
